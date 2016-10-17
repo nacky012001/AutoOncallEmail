b160b8703e2c22ad351f439c29e2923746821f9e
kin.ho <kin.ho@laxino.com>
2016-10-17 11:05:22 +0800

    updated Gemile and send oncall email.sh

--- Gemfile
+++ Gemfile
@@ -1,1 +1,3 @@
+source 'https://rubygems.org/'
+
 gem 'mail', '2.4.4'
--- send_oncall_email.sh
+++ send_oncall_email.sh
@@ -1,2 +1,3 @@
+BASEDIR=$(dirname "$0")
 . /opt/deploy/env/ruby-1.9.3/.envrc
-ruby /opt/deploy/env/cronjobs/send_oncall_email/send_oncall_email.rb
+ruby "$BASEDIR/send_oncall_email.rb"
